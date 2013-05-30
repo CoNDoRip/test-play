5f559a277acdfb93b678d0941621814c446a768a
Idel <cndr.ip@gmail.com>
2013-05-30 22:28:56 +0400

    Add messages and small fixes in config files

--- project/Build.scala
+++ project/Build.scala
@@ -4,20 +4,20 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "test-play"
-  val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "test-play"
+    val appVersion      = "1.0"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean,
-    "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final"
-  )
+    val appDependencies = Seq(
+    // Add your project dependencies here
+      javaCore,
+      javaJdbc,
+      javaJpa,
+      "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final"
+    )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-    ebeanEnabled := false
-  )
+      ebeanEnabled := false   
+    )
 
 }
