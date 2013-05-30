3bc830baf3fe5d888fdb851e753f5b2343a2680f
Idel <cndr.ip@gmail.com>
2013-05-30 22:30:23 +0400

    Add first page: list of employees

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,51 @@
 package controllers;
 
+import java.util.*;
+
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+import play.db.jpa.*;
 
 import views.html.*;
 
+import models.*;
+
+/**
+ * Manage a database of employees
+ */
 public class Application extends Controller {
-  
+    
+    /**
+     * Handle default path requests, redirect to employees list
+     */
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return GO_HOME;
     }
-  
+
+    /**
+     * This result directly redirect to application home.
+     */
+    public static Result GO_HOME = redirect(
+        routes.Application.list(0, "id", "asc", "")
+    );
+
+    /**
+     * Display the paginated list of employees.
+     *
+     * @param page Current page number (starts from 0)
+     * @param sortBy Column to be sorted
+     * @param order Sort order (either asc or desc)
+     * @param filter Filter applied on employee last names
+     */
+    @Transactional(readOnly=true)
+    public static Result list(int page, String sortBy, String order, String filter) {
+        return ok(
+            list.render(
+                Employee.page(page, 10, sortBy, order, filter),
+                sortBy, order, filter
+            )
+        );
+    }
 }
---
+++ app/models/Employee.java
@@ -0,0 +1,134 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.data.format.*;
+import play.data.validation.*;
+
+import play.db.jpa.*;
+
+/**
+ * Employee entity managed by JPA
+ */
+@Entity 
+@SequenceGenerator(name = "employee_seq", sequenceName = "employee_seq")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_seq")
+    public Long id;
+    
+    @Constraints.Required
+    public String first_name;
+    
+    @Constraints.Required
+    public String last_name;
+    
+    public String second_name;
+    
+    public Short age;
+    
+    public String experience;
+    
+    public String description;
+
+    /**
+     * Find an employee by id.
+     */
+    public static Employee findById(Long id) {
+        return JPA.em().find(Employee.class, id);
+    }
+    
+    /**
+     * Insert this new employee.
+     */
+    public void save() {
+        this.id = id;
+        JPA.em().persist(this);
+    }
+
+    /**
+     * Update this employee.
+     */
+    public void update(Long id) {
+        this.id = id;
+        JPA.em().merge(this);
+    }
+    
+    /**
+     * Delete this employee.
+     */
+    public void delete() {
+        JPA.em().remove(this);
+    }
+
+    /**
+     * Return a page of employees
+     *
+     * @param page Page to display
+     * @param pageSize Number of employees per page
+     * @param sortBy Employee property used for sorting
+     * @param order Sort order (either or asc or desc)
+     * @param filter Filter applied on the last_name column
+     */
+    public static Page page(int page, int pageSize, String sortBy, String order, String filter) {
+        if(page < 1) page = 1;
+        Long total = (Long)JPA.em()
+            .createQuery("SELECT count(e) FROM Employee e WHERE lower(e.last_name) like ?")
+            .setParameter(1, "%" + filter.toLowerCase() + "%")
+            .getSingleResult();
+        List<Employee> data = JPA.em()
+            .createQuery("FROM Employee e WHERE lower(e.last_name) like ? order by e." + sortBy + " " + order)
+            .setParameter(1, "%" + filter.toLowerCase() + "%")
+            .setFirstResult((page - 1) * pageSize)
+            .setMaxResults(pageSize)
+            .getResultList();
+        return new Page(data, total, page, pageSize);
+    }
+    
+    /**
+     * Used to represent a computers page.
+     */
+    public static class Page {
+        
+        private final int pageSize;
+        private final long totalRowCount;
+        private final int pageIndex;
+        private final List<Employee> list;
+        
+        public Page(List<Employee> data, long total, int page, int pageSize) {
+            this.list = data;
+            this.totalRowCount = total;
+            this.pageIndex = page;
+            this.pageSize = pageSize;
+        }
+        
+        public long getTotalRowCount() {
+            return totalRowCount;
+        }
+        
+        public int getPageIndex() {
+            return pageIndex;
+        }
+        
+        public List<Employee> getList() {
+            return list;
+        }
+        
+        public boolean hasPrev() {
+            return pageIndex > 1;
+        }
+        
+        public boolean hasNext() {
+            return (totalRowCount/pageSize) >= pageIndex;
+        }
+        
+        public String getDisplayXtoYofZ() {
+            int start = ((pageIndex - 1) * pageSize + 1);
+            int end = start + Math.min(pageSize, list.size()) - 1;
+            return start + " to " + end + " of " + totalRowCount;
+        }
+        
+    }
+}
